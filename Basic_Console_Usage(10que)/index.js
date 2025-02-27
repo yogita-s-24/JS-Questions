4e09907d9d4aae35b07c0ae5f5b2d2e5b19685ed
Yogita Shete <yogitashete24@gmail.com>
2025-02-27 22:24:02 +0530

    Solve the basic 10 questions of the js

---
+++ Basic_Console_Usage(10que)/index.js
@@ -0,0 +1,38 @@
+// 1. Log your name and favorite hobby to the console.
+console.log("Yogita Shete");
+console.log("Daily coding and enchance new skills");
+
+//2. Perform and log result of the 45*2-10.
+console.log(45 * 2 - 10);
+
+//3. Use the console.log() to display the current year.
+const dt = new Date();
+console.log(dt.getFullYear());
+
+//4. Create two variables for first and last name. Concatenate anf log them.
+const firstName = "Yogita";
+const lastName = "Shete";
+console.log(`${firstName} ${lastName}`);
+
+//5. Track the value of the variable by logging it before and after updating.
+let a = 20;
+console.log(a);
+a = 30;
+console.log(a);
+
+//6.Use console.log() to simulate an error message.
+console.log("Something went to wrong");
+
+//7.Log the square of the 12 to the console.
+console.log(12*12);
+
+//8.Print the type of the variable holding the value true.
+const b = true;
+console.log(typeof b);
+
+//9. Create the variable holding your age and log whether its greater than 18.
+const age = 19;
+if(age > 18) console.log(true)
+
+//10. Log the result of 100/0 and observe the output.
+console.log(100/0);
