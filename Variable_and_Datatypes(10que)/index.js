1f22eaedd55d65a2eda2862e56f2ebf0480417f2
Yogita Shete <yogitashete24@gmail.com>
2025-02-28 11:33:53 +0530

    Declare and log various JavaScript variables, including let, const, and different data types. Demonstrate reassignment, type checking, and observe const behavior with arrays.

---
+++ Variable_and_Datatypes(10que)/index.js
@@ -0,0 +1,41 @@
+//11. Declare a variable using let and log this value.
+let value = 24;
+console.log(value);
+
+//12. Create a constant variable to store the value of the PI AND LOG it.
+const PI = Math.PI;
+console.log(PI);
+
+//13. Reassign a value toa variable declared with let and log the result.
+let a = 40;
+a = 90;
+console.log(a);
+
+//14. Check the type of null and log it.
+console.log(typeof null);
+
+//15.Create a variable with a number as a string (e.g "25") and log their type.
+let num = "25";
+console.log(typeof num);
+
+//16.Use typeof to check the type of a boolean variable.
+const isBoolean = true;
+console.log(typeof isBoolean);
+
+//17. Create the three variable of thats types string, number and boolean and log their values.
+const val = "24";
+const number = 43;
+const isNan = false;
+console.log(`${val} ${number} ${isNan}`);
+
+//18. Declare the variable without assigning the value.
+let y;
+console.log(y);
+
+//19. Create a variable with undefined and log its type
+let z = undefined;
+console.log(typeof z);
+
+//20. Use const to create an array. Try reassigning the array and observe the error.
+const arr = [];
+arr = [];
